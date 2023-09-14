f5ca8e75a43795e7e667af0860b03595054b216a
kyhchn <alvinkn.dev@gmail.com>
2023-09-14 15:14:57 +0700

    add socmed and tech stacks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,67 @@
-'use client'
-import Image from "next/image"
-import projects from "@/data/projects.json"
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from "react"
-import ProjectItem from "@/components/projectItem"
-import TechStack from "@/components/techStack"
-import SocialButton from "@/components/socialButton"
+"use client";
+import Image from "next/image";
+import projects from "@/data/projects.json";
+import { TbSchool } from "react-icons/tb";
+import { FaLocationDot } from "react-icons/fa6";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import ProjectItem from "@/components/projectItem";
+import TechStack from "@/components/techStack";
+import SocialButton from "@/components/socialButton";
 export default function Home() {
   const socialButtons = [
-    { src: '/github.svg', alt: 'GitHub', uri: 'https://github.com/kyhchn' },
-    { src: '/linkedin.svg', alt: 'LinkedIn', uri: 'https://www.linkedin.com/in/alvinkn/' },
-    { src: '/instagram.svg', alt: 'Instagram', uri: 'https://www.instagram.com/vvinalvinn/' },
-    { src: '/gmail.svg', alt: 'Gmail', uri: 'mailto:alvinkn.dev@gmail.com' }
+    { src: "/github.svg", alt: "GitHub", uri: "https://github.com/kyhchn" },
+    {
+      src: "/linkedin.svg",
+      alt: "LinkedIn",
+      uri: "https://www.linkedin.com/in/alvinkn/",
+    },
+    {
+      src: "/instagram.svg",
+      alt: "Instagram",
+      uri: "https://www.instagram.com/vvinalvinn/",
+    },
+    { src: "/gmail.svg", alt: "Gmail", uri: "mailto:alvinkn.dev@gmail.com" },
   ];
   useEffect(() => {
     AOS.init({
-      duration: 1000
-    })
-  }, [])
+      duration: 1000,
+    });
+  }, []);
   return (
     <>
       <section id="about-me">
         <div className="flex h-[75vh] md:h-screen flex-col gap-3 md:flex-row bg-white items-center p-10 md:p-20">
-          <div className="md:flex-1 text-2xl md:text-5xl font-bold text-blue-900" data-aos='fade-right'>
-            <h1> Hi There, <br />
-              Im <span className="text-purple-400">Alvin Kamal Nasich</span></h1>
-            <p className="text-base text-black mt-3" data-aos='zoom-in' >
-              I am into  <span className="text-red-700 md:text-2xl">Front End Development</span>
+          <div
+            className="md:flex-1 text-2xl md:text-5xl font-bold text-blue-900"
+            data-aos="fade-right"
+          >
+            <h1>
+              {" "}
+              Hi There, <br />
+              Im <span className="text-purple-400">Alvin Kamal Nasich</span>
+            </h1>
+            <p className="text-base text-black mt-3" data-aos="zoom-in">
+              I am into{" "}
+              <span className="text-red-700 md:text-2xl">
+                Front End Development
+              </span>
             </p>
-            <div className="flex mt-5 gap-3 justify-center md:justify-start" data-aos="fade-up">
+            <div className="flex flex-col text-black" data-aos="zoom-in">
+              <div className="flex flex-row text-base gap-3 items-center">
+                <TbSchool className="w-8 h-8" />
+                <p>Information System student at Brawijaya University</p>
+              </div>
+              <div className="flex flex-row text-base gap-3 items-center">
+                <FaLocationDot className="w-8 h-8" />
+                <p>Indonesia</p>
+              </div>
+            </div>
+            <div
+              className="flex mt-5 gap-3 justify-center md:justify-start"
+              data-aos="fade-up"
+            >
               {socialButtons.map((button, index) => (
                 <SocialButton
                   key={index}
@@ -41,7 +73,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex-1 px-2 md:h-screen w-full" data-aos='fade-left'>
+          <div className="flex-1 px-2 md:h-screen w-full" data-aos="fade-left">
             <div className="h-full w-full md:w-5/6 relative justify-center">
               <Image src="/alvin.png" alt="hero" fill objectFit="contain" />
             </div>
@@ -51,7 +83,10 @@ export default function Home() {
 
       <section id="projects" className="overflow-x-hidden">
         <div className="flex flex-col justify-start bg-slate-800 px-5 py-10">
-          <div className="text-white text-4xl mb-5 text-center" data-aos="zoom-out">
+          <div
+            className="text-white text-4xl mb-5 text-center"
+            data-aos="zoom-out"
+          >
             <h1>My Projects</h1>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
@@ -63,7 +98,7 @@ export default function Home() {
                 title={project.title}
                 desc={project.desc}
                 alt={project.alt}
-                aosData={index % 2 == 0 ? 'fade-right' : 'fade-left'}
+                aosData={index % 2 == 0 ? "fade-right" : "fade-left"}
               />
             ))}
           </div>
@@ -72,22 +107,44 @@ export default function Home() {
 
       <section id="tech-stack" className="overflow-x-hidden">
         <div className="flex flex-col justify-center bg-white px-5 py-10">
-          <div className="text-black text-4xl mb-5 text-center" data-aos="zoom-out">
+          <div
+            className="text-black text-4xl mb-5 text-center"
+            data-aos="zoom-out"
+          >
             <h1>Tech Stack</h1>
           </div>
           <div className="grid grid-cols-2 md:grid-cols-5" data-aos="flip-up">
-            <TechStack image="https://storage.googleapis.com/cms-storage-bucket/4fd5520fe28ebf839174.svg" stack="Flutter" />
-            <TechStack image="https://firebase.google.com/downloads/brand-guidelines/SVG/logo-logomark.svg" stack="Firebase" />
-            <TechStack image="https://laravel.com/img/logomark.min.svg" stack="Laravel" />
+            <TechStack
+              image="https://storage.googleapis.com/cms-storage-bucket/4fd5520fe28ebf839174.svg"
+              stack="Flutter"
+            />
+            <TechStack
+              image="https://firebase.google.com/downloads/brand-guidelines/SVG/logo-logomark.svg"
+              stack="Firebase"
+            />
+            <TechStack
+              image="https://laravel.com/img/logomark.min.svg"
+              stack="Laravel"
+            />
             <TechStack image="/next.svg" stack="Next JS" />
             <TechStack image="/tailwind.svg" stack="Tailwind" />
-            <TechStack image="https://railway.app/brand/logo-dark.svg" stack="Railway" />
+            <TechStack
+              image="https://railway.app/brand/logo-dark.svg"
+              stack="Railway"
+            />
             <TechStack image="/github.svg" stack="Github" />
-            <TechStack image="https://wiki.postgresql.org/images/a/a4/PostgreSQL_logo.3colors.svg" stack="PostgreSQL" />
+            <TechStack
+              image="https://wiki.postgresql.org/images/a/a4/PostgreSQL_logo.3colors.svg"
+              stack="PostgreSQL"
+            />
+            <TechStack image="/hardhat.svg" stack="Hardhat" />
+            <TechStack
+              image="https://upload.wikimedia.org/wikipedia/commons/9/98/Solidity_logo.svg"
+              stack="Solidity"
+            />
           </div>
         </div>
       </section>
     </>
-
-  )
+  );
 }
--- src/components/projectItem.tsx
+++ src/components/projectItem.tsx
@@ -1,24 +1,37 @@
 import Link from "next/link";
 
 type ProjectItemProps = {
-    href: string;
-    img: string;
-    title: string;
-    desc: string;
-    alt: string;
-    aosData: string;
+  href: string;
+  img: string;
+  title: string;
+  desc: string;
+  alt: string;
+  aosData: string;
 };
 
 export default function ProjectItem(props: ProjectItemProps) {
-    const { href, img, title, desc, aosData, alt } = props;
+  const { href, img, title, desc, aosData, alt } = props;
 
-    return (
-        <Link href={href} passHref className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-max hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 px-3" data-aos={aosData}>
-            <img className="object-contain w-full rounded-t-lg h-48 md:w-48 md:rounded-none md:rounded-l-lg" src={img} alt={alt} />
-            <div className="flex flex-col justify-between p-4 leading-normal text-center md:text-start">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{desc}</p>
-            </div>
-        </Link>
-    );
+  return (
+    <Link
+      href={href}
+      passHref
+      className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 px-3"
+      data-aos={aosData}
+    >
+      <img
+        className="object-contain w-full rounded-t-lg h-48 md:w-48 md:rounded-none md:rounded-l-lg"
+        src={img}
+        alt={alt}
+      />
+      <div className="flex flex-col justify-between p-4 leading-normal text-center md:text-start">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {desc}
+        </p>
+      </div>
+    </Link>
+  );
 }
